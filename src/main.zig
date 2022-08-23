391a45224dd6e929c96fdc8b6769b07e298a7eea
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-08-23 10:01:06 +0900

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -9,12 +9,27 @@
 
 const std = @import("std");
 
+/// コンフィギュレーション構造体
+const GlobalCotext = struct {
+    /// 文字列
+    field1: []const u8 = "",
+
+    /// 整数
+    field2: u32 = 123,
+};
+
+/// mutable
+var _ctx = GlobalCotext{};
+
 /// Zig のエントリーポイントです。
 ///
 /// WebAssembly を出力する場合、意味はありません。(消しても OK)
 pub fn main() anyerror!void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("ハロー WASM!\n", .{});
+    try stdout.print("u32: [{}]\n", .{_ctx.field2});
+    _ctx.field2 = _ctx.field2 + 1;
+    try stdout.print("u32: [{}]\n", .{_ctx.field2});
 }
 
 /// 何らかの整数を返します。
@@ -25,4 +40,9 @@ export fn test1() usize {
 /// `left` と `right` を足した数を返します。
 export fn test2(left: usize, right: usize) usize {
     return left + right;
+}
+
+/// コンテキストオブジェクトを公開できるか？(できてない)
+export fn context() *GlobalCotext {
+    return &_ctx;
 }
